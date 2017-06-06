5ed5eba033d9bba43cd3b5c808517b890e321261
Cristian Leu <cristian.leu@fortech.ro>
2017-06-06 10:06:35 +0300

    added baberc npm5 and yarn locks

---
+++ src/js/components/Layout.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default class Layout extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            title: "It works!",
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <p>This is a test para</p>
+                <h1>{ this.state.title }</h1>
+            </div>
+        );
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const debug = process.env.NODE_ENV !== "production";
+const webpack = require( "webpack" );
+const path = require( "path" );
+
+module.exports = {
+    context: path.join( __dirname, "src" ),
+    devtool: debug ? "inline-sourcemap" : false,
+    entry: "./js/App.js",
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                enforce: "pre",
+                exclude: /(node_modules|bower_components)/,
+                use: [
+                    {
+                        loader: "eslint-loader",
+                        options: {
+                            failOnWarning: false,
+                            failOnError: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader",
+                /*query: {
+                    presets: [ "react", "es2015", "stage-0" ],
+                },*/
+            },
+        ],
+    },
+    output: {
+        path: `${ __dirname }/src/`,
+        filename: "bundle.js",
+    },
+    plugins: debug ? [] : [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin( { mangle: false, sourcemap: false } ),
+    ],
+};
